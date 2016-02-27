7fd56d3d1710087f2a2a29e91fdf57730f3830f8
Jose Pablo Granados <jpgcodecr@gmail.com>
2016-02-27 12:13:23 -0600

    Adding player key behaviors and sprites

--- app/js/play.js
+++ app/js/play.js
@@ -6,13 +6,35 @@ IngameState.prototype = {
         this.game.load.image('starsBg', 'assets/images/game_bg_universe.jpg');
         this.game.load.image('lava', 'assets/images/lava_bg.png');
         this.game.load.image('rockPlatform', 'assets/images/rock_platform.png');
+        this.game.load.spritesheet('playerIdle','assets/images/character1/idle.png', 75.3, 102, 6);
+        this.game.load.image('playerJump', 'assets/images/character1/jump.png');
+        this.game.load.audio('jump', 'assets/sounds/jump.wav');
     },
     
     create: function() {
+        this.cursors = this.game.input.keyboard.createCursorKeys();
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
         this.game.add.sprite(0, 0, 'starsBg');
         this.lava = this.game.add.tileSprite(0, 530, this.game.stage.game.width, 292, 'lava');
         this.createPlatforms();
-        this.animatePlatforms();
+        //this.animatePlatforms();
+        this.createPlayer();
+        this.jumpSound = this.game.add.audio('jump');
+
+    },
+
+    createPlayer: function(){
+        this.player = this.game.add.sprite(50, 420, 'playerIdle');
+        this.player.animations.add('idle', [0,1,2,3,4,5], 20, true);
+        this.player.animations.play('idle');
+        // Enable physics
+        this.game.physics.arcade.enable(this.player);
+        this.player.body.gravity.y = 400;
+        this.player.body.bounce.y = 0.2;
+        this.player.body.collideWorldBounds = true; 
+        this.player.anchor.setTo(0.5, 1);
+        //this.player.body.setSize(187, 50, 0, 20);
     },
 
     createPlatforms: function(){
@@ -30,28 +52,62 @@ IngameState.prototype = {
 
         for (var i = 0; i < 5; i++) {
             var platform = this.platforms.create(startingCords[i].left, startingCords[i].top, 'rockPlatform');
+            platform.body.immovable = true;
         }
 
     },
 
     animatePlatforms: function(){
-        this.platforms.forEach(function(platform){
 
-            var goDown = function(){
-                var tween = this.game.add.tween(platform).to({ y: platform.position.y + 100 }, 2000, Phaser.Easing.Quadratic.InOut, true, 0);
-                tween.onComplete.add(goUp, this);
-            }
+        var platforms = this.platforms;        
 
-            var goUp = function(){
-                var tween2 = this.game.add.tween(platform).to({ y: platform.position.y - 100 }, 2000, Phaser.Easing.Quadratic.InOut, true, 0);
-                tween2.onComplete.add(goDown, this); 
-            }
+        var goDown = function(){
+            var tween = this.game.add.tween(platforms).to({ y: platforms.position.y + 100 }, 3000, Phaser.Easing.Quadratic.InOut, true, 0);
+            tween.onComplete.add(goUp, this);
+        }
 
-            goDown();
-        });
+        var goUp = function(){
+            var tween2 = this.game.add.tween(platforms).to({ y: platforms.position.y - 100 }, 3000, Phaser.Easing.Quadratic.InOut, true, 0);
+            tween2.onComplete.add(goDown, this); 
+        }
+
+        goDown();
+        
     },
     
     update: function() {
         this.lava.tilePosition.x += 0.2;
+        this.game.physics.arcade.collide(this.player, this.platforms);
+        var grounded = this.player.body.touching.down;
+        if(grounded){
+            this.player.loadTexture('playerIdle', 0);
+            this.player.animations.play('idle');    
+        }
+        
+        if (this.cursors.up.isDown) {
+            if(grounded){
+                this.player.loadTexture('playerJump', 0);
+                this.player.body.velocity.y = -250;
+                this.jumpSound.play();
+            }
+        }
+
+        if (this.cursors.left.isDown) {
+            this.player.scale.x = -1;
+            if (grounded){
+                this.player.body.velocity.x = -150;
+            }
+            this.player.body.velocity.x = -150;
+        } else if (this.cursors.right.isDown) {
+            this.player.scale.x = 1;
+            if (grounded){
+                this.player.body.velocity.x = 150;
+            }
+        } else {
+            if (grounded){
+                this.player.body.velocity.x = 0;
+            }
+        }
+
     }
 };
